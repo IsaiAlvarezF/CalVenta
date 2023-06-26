31e20f6f7b6502be0e63f61eb0e164660bfb22cb
Carlos Sura <csura@briloerp.com>
2023-06-26 07:25:29 -0600

    Cambio de prueba

--- Calcular/Form1.Designer.vb
+++ Calcular/Form1.Designer.vb
@@ -22,155 +22,155 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Button3 = New System.Windows.Forms.Button()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
-        Me.SuspendLayout()
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(127, 202)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.ReadOnly = True
-        Me.TextBox2.Size = New System.Drawing.Size(173, 23)
-        Me.TextBox2.TabIndex = 10
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(127, 26)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(146, 15)
-        Me.Label1.TabIndex = 3
-        Me.Label1.Text = "CALCULADORA DE VENTA"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(69, 253)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "Calcular "
-        Me.Button1.UseVisualStyleBackColor = True
-        '
-        'Button2
-        '
-        Me.Button2.Location = New System.Drawing.Point(161, 253)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
-        Me.Button2.TabIndex = 5
-        Me.Button2.Text = "Limpiar "
-        Me.Button2.UseVisualStyleBackColor = True
-        '
-        'ComboBox1
-        '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Empleado", "Cliente"})
-        Me.ComboBox1.Location = New System.Drawing.Point(127, 68)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 23)
-        Me.ComboBox1.TabIndex = 1
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(25, 68)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(84, 15)
-        Me.Label2.TabIndex = 9
-        Me.Label2.Text = "Tipo de cliente"
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(25, 126)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(84, 15)
-        Me.Label3.TabIndex = 10
-        Me.Label3.Text = "Ingrese monto"
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(25, 210)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(70, 15)
-        Me.Label4.TabIndex = 11
-        Me.Label4.Text = "Precio total "
-        '
-        'Button3
-        '
-        Me.Button3.Location = New System.Drawing.Point(266, 253)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
-        Me.Button3.TabIndex = 6
-        Me.Button3.Text = "Salir"
-        Me.Button3.UseVisualStyleBackColor = True
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(127, 126)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(173, 23)
-        Me.TextBox1.TabIndex = 2
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(25, 171)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(78, 15)
-        Me.Label5.TabIndex = 12
-        Me.Label5.Text = "Precio sin IVA"
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(127, 168)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.ReadOnly = True
-        Me.TextBox3.Size = New System.Drawing.Size(173, 23)
-        Me.TextBox3.TabIndex = 13
-        '
-        'Form1
-        '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
-        Me.ClientSize = New System.Drawing.Size(369, 315)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.TextBox2)
-        Me.ForeColor = System.Drawing.SystemColors.InactiveCaptionText
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
-        Me.Name = "Form1"
-        Me.Text = "Cal "
-        Me.ResumeLayout(False)
-        Me.PerformLayout()
-
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        TipoCliente = New ComboBox()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Button3 = New Button()
+        TextBox1 = New TextBox()
+        Label5 = New Label()
+        TextBox3 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(127, 202)
+        TextBox2.Name = "TextBox2"
+        TextBox2.ReadOnly = True
+        TextBox2.Size = New Size(173, 23)
+        TextBox2.TabIndex = 10
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(127, 26)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(146, 15)
+        Label1.TabIndex = 3
+        Label1.Text = "CALCULADORA DE VENTA"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(69, 253)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 3
+        Button1.Text = "Calcular "
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(161, 253)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 5
+        Button2.Text = "Limpiar "
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' TipoCliente
+        ' 
+        TipoCliente.DropDownStyle = ComboBoxStyle.DropDownList
+        TipoCliente.FormattingEnabled = True
+        TipoCliente.Items.AddRange(New Object() {"Empleado", "Cliente"})
+        TipoCliente.Location = New Point(127, 68)
+        TipoCliente.Name = "TipoCliente"
+        TipoCliente.Size = New Size(121, 23)
+        TipoCliente.TabIndex = 1
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(25, 71)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(84, 15)
+        Label2.TabIndex = 9
+        Label2.Text = "Tipo de cliente"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(25, 126)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(84, 15)
+        Label3.TabIndex = 10
+        Label3.Text = "Ingrese monto"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(25, 210)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(70, 15)
+        Label4.TabIndex = 11
+        Label4.Text = "Precio total "
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(266, 253)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(75, 23)
+        Button3.TabIndex = 6
+        Button3.Text = "Salir"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(127, 126)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(173, 23)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(25, 171)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(78, 15)
+        Label5.TabIndex = 12
+        Label5.Text = "Precio sin IVA"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(127, 168)
+        TextBox3.Name = "TextBox3"
+        TextBox3.ReadOnly = True
+        TextBox3.Size = New Size(173, 23)
+        TextBox3.TabIndex = 13
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.GradientActiveCaption
+        ClientSize = New Size(369, 315)
+        Controls.Add(TextBox3)
+        Controls.Add(Label5)
+        Controls.Add(Button3)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(TipoCliente)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Controls.Add(TextBox2)
+        ForeColor = SystemColors.InactiveCaptionText
+        FormBorderStyle = FormBorderStyle.Fixed3D
+        Name = "Form1"
+        Text = "Cal "
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
-    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TipoCliente As ComboBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
--- Calcular/Form1.vb
+++ Calcular/Form1.vb
@@ -1,40 +1,10 @@
 ﻿Public Class Form1
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
-
-    End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub ComboBox1_SelectedIndexChanged_1(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
-
-    End Sub
-
-    Private Sub TextBox1_TextChanged_1(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         Me.TextBox1.Text = Nothing
         Me.TextBox2.Text = Nothing
         Me.TextBox3.Text = Nothing
-        Me.ComboBox1.Text = Nothing
+        Me.TipoCliente.Text = Nothing
     End Sub
-
-
 
     Private Sub Form1(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Habilitar el reconocimiento de la tecla Enter
@@ -45,8 +15,8 @@
     Private Sub Calcular()
         Try
             Dim MontoInicial, ValorTotalSinIva, Total As Double
-            If ComboBox1.SelectedItem IsNot Nothing Then
-                Dim Op As String = ComboBox1.SelectedItem.ToString()
+            If TipoCliente.SelectedItem IsNot Nothing Then
+                Dim Op As String = TipoCliente.SelectedItem.ToString()
                 MontoInicial = Double.Parse(Me.TextBox1.Text)
                 If (Op = "Empleado") Then
                     ValorTotalSinIva = MontoInicial / 0.9
